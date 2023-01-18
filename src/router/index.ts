34aebc1870a6457337d0a8a72423e34abfbd1a1d
ntua-el19181 <el19181@mail.ntua.gr>
2023-01-19 01:11:55 +0200

    hello touloubes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,10 @@
+// @ts-ignore
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+// @ts-ignore
+// @ts-ignore
+// @ts-ignore
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
