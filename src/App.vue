1fa2f486545e6d69ec8bbb5dced38c20191df1e3
gerasmark <gerasmark@hotmail.gr>
2023-01-21 16:31:57 +0200

    a

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 <template>
     <form @submit.prevent="addSurvey">
       <input v-model="name" placeholder="Survey Name" required>
+      <input v-model="questions" placeholder="Your Question" required>
       <input v-model="creator" placeholder="Your Name" required>
       <button type="submit">Add Survey</button>
     </form>
