a2d88987181d37a269aaed198f712ebad1d6a370
ntua-el19192 <116566196+ntua-el19192@users.noreply.github.com>
2023-02-01 16:24:37 +0200

    usersurveys

---
+++ src/components/UserSurveys.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-app id="inspire">
+        <v-app-bar app color=" bg-indigo-accent-3 " class="pa-0" :elevation="5">
+            <v-col class="d-flex justify-space-around">
+                <h1>IntelliQ</h1>
+            </v-col>
+        </v-app-bar>
+
+        <v-container class="text-center">
+            <h1>
+                Εδω θελω να βγαινουν οι ερευνες
+            </h1>
+
+        </v-container>
+
+
+    </v-app>
+</template>
+
+<script>
+export default {
+    name: "UserSurveys"
+}
+
+
+</script>
+
+<style scoped>
+
+</style>
