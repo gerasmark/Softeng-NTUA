2bcb0fb0cd4b1548ec25ad5c69a6f3ac265746ba
gerasmark <gerasmark@hotmail.gr>
2023-02-01 17:06:53 +0200

    routes and controllers check

--- src/postservice.js
+++ src/postservice.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-const url = 'http://localhost:9103/post/user/';
-const url1 = 'http://localhost:9103/post/admin/';
-const url2 = 'http://localhost:9103/post/institution/';
+const url = 'http://localhost:9103/intelliq_api/user/';
+const url1 = 'http://localhost:9103/intelliq_api/admin/';
+const url2 = 'http://localhost:9103/intelliq_api/institution/';
 
 class postService {
     static getuser(){
