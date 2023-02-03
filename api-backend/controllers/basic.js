e23cfedc084ee3a858f2825a6e936265837022ea
gerasmark <gerasmark@hotmail.gr>
2023-02-03 16:26:50 +0200

    routes and controllers check

--- api-backend/controllers/basic.js
+++ api-backend/controllers/basic.js
@@ -16,5 +16,15 @@ exports.getQuestionnaireQuestion = async (req, res) => {
 
 }
 exports.postQuestionnaire = async (req, res) => {
+    const questionnaireID = req.params.questionnaireID;
+    const questionID = req.params.questionID;
+    const session = req.params.session;
+    const optionID = req.params.optionID;
+    //res.send( await answerModel.find({ session:session,questionnaireID:questionnaireID},{answers: { $elemMatch: { qID:questionID}}}));
+    answerModel.findOneAndUpdate({ session:session,questionnaireID:questionnaireID,answers:  { qID:questionID}}, {$set: {answers:{ans:optionID }}}, function (err, result) {
+        if (err) {res.send({ status: "fail",r:err,re:result});}
+        else {res.send({ status: "success" ,re:result });}
+    });
+
 
 }
