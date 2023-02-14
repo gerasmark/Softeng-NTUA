ee9773b7dc457531c8be1a406d1a95f6af398b0c
ntua-el19181 <el19181@mail.ntua.gr>
2023-02-14 19:10:19 +0200

    fix answers

--- src/components/CreateSurvey.vue
+++ src/components/CreateSurvey.vue
@@ -9,7 +9,7 @@
         <div class="mt-10 text-center">
             <h1> Create a Survey</h1>
             <div style="max-width: 600px; margin: 0 auto;">
-                <v-card class="my-10" color="indigo-lighten-5"  v-for="(question, index) in questions" :key="index">
+                <v-card class="my-10" color="indigo-lighten-5" v-for="(question, index) in questions" :key="index">
                     <v-text-field label="Question ID" v-model="question.questionID" disabled/>
 
                     <v-select
@@ -21,33 +21,32 @@
                     <template v-if="question.type!=='end'">
 
                         <v-text-field label="Question" class="text-light-blue-darken-4" v-model="question.text"/>
-
-                        <div v-for="(answer, index) in questions[this.currentQuestionIndex].answers" :key="index">
-                            <v-text-field class="text-indigo-darken-4" label="Answer" v-model="answer.text"/>
-                            <v-select
-                                    class="text-indigo-darken-4"
-                                    label="Next Question ID"
-                                    v-model="answer.nextQuestionID"
-                                    :items="questionIDs"
-                            />
-                        </div>
+                            <div v-for="(answer, index) in question.answers" :key="index">
+                                <v-text-field class="text-indigo-darken-4" label="Answer" v-model="answer.text"/>
+                                <v-select
+                                        class="text-indigo-darken-4"
+                                        label="Next Question ID"
+                                        v-model="answer.nextQuestionID"
+                                        :items="questionIDs"
+                                />
+                            </div>
 
                         <v-btn @click="addAnswer" class="bg-indigo-darken-4">
                             <v-icon>mdi-plus</v-icon>
                             Add Answer
                         </v-btn>
-<!--                        <v-text-field label="Answer 2" v-model="question.answers[1].text"/>-->
-<!--                        <v-select-->
-<!--                                label="Next Question ID"-->
-<!--                                v-model="question.answers[1].nextQuestionID"-->
-<!--                                :items="questionIDs"-->
-<!--                        />-->
-<!--                        <v-text-field label="Answer 3" v-model="question.answers[2].text"/>-->
-<!--                        <v-select-->
-<!--                                label="Next Question ID"-->
-<!--                                v-model="question.answers[2].nextQuestionID"-->
-<!--                                :items="questionIDs"-->
-<!--                        />-->
+                        <!--                        <v-text-field label="Answer 2" v-model="question.answers[1].text"/>-->
+                        <!--                        <v-select-->
+                        <!--                                label="Next Question ID"-->
+                        <!--                                v-model="question.answers[1].nextQuestionID"-->
+                        <!--                                :items="questionIDs"-->
+                        <!--                        />-->
+                        <!--                        <v-text-field label="Answer 3" v-model="question.answers[2].text"/>-->
+                        <!--                        <v-select-->
+                        <!--                                label="Next Question ID"-->
+                        <!--                                v-model="question.answers[2].nextQuestionID"-->
+                        <!--                                :items="questionIDs"-->
+                        <!--                        />-->
                         <v-spacer></v-spacer>
                         <v-spacer></v-spacer>
                         <v-btn @click="addQuestion" class="bg-indigo-darken-4">
@@ -56,7 +55,7 @@
                         </v-btn>
                     </template>
                     <template v-else>
-                        <v-text-field label="End message" v-model="endmessage" />
+                        <v-text-field label="End message" v-model="endmessage"/>
                         <v-btn color="indigo" @click="Submit()">Submit</v-btn>
                     </template>
 
@@ -103,7 +102,7 @@ export default {
                     Number(lastQuestion.questionID.slice(2)) + 1
             ).padStart(2, '0')}`;
             this.currentQuestionIndex++;
-            console.log( this.currentQuestionIndex);
+            console.log(this.currentQuestionIndex);
             this.questions.push({
                 questionID: newQuestionID,
                 text: '',
@@ -115,9 +114,9 @@ export default {
                 ],
             });
         },
-        addAnswer(){
+        addAnswer() {
             this.currentQuestionIndex = this.questions.length - 1;
-            this.questions[this.currentQuestionIndex].answers.push({ text: '', nextQuestionID: '-' });
+            this.questions[this.currentQuestionIndex].answers.push({text: '', nextQuestionID: '-'});
             console.log(this.questions[this.currentQuestionIndex].answers);
             console.log(this.questions[this.currentQuestionIndex].answers);
         },
@@ -129,7 +128,8 @@ export default {
             }
         },
         Submit() {
-            this.$router.push({path: '/admin/'+this.$route.params.id})
+            console.log(this.questions);
+            this.$router.push({path: '/admin/' + this.$route.params.id})
         }
     },
     created() {
