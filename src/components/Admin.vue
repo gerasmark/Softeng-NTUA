e4fd7b22db215b351b23687f135771e09b4f566c
ntua-el19192 <116566196+ntua-el19192@users.noreply.github.com>
2023-02-15 16:41:08 +0200

    ViewAnswers.vue almost done, displaying the text of the answers left,

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,51 +1,45 @@
 <template>
-    <v-app id="inspire">
+    <v-app>
         <v-app-bar app color="indigo" class="pa-0" :elevation="5">
             <v-col class="d-flex justify-space-around">
                 <h1>IntelliQ</h1>
             </v-col>
         </v-app-bar>
-        <div id="app" style="position:absolute; top:55px; left:35px">
-            <v-container>
-                <v-row>
-                    <v-col class="text-left mr-10"><h2>Welcome {{ admin }}</h2></v-col>
-                    <v-col class="text-right">
-                        <v-btn @click="createSurvey(admin)">Create a Survey</v-btn>
-                    </v-col>
-                </v-row>
-            </v-container>
-        </div>
-        <v-container class="fill-height justify-lg-space-around" fluid>
-            <v-row align="center" justify="center" dense>
+
+        <v-container>
+            <v-row>
+                <v-col cols="12" md="6">
+                    <h2 class="text-left mt-7">Welcome {{ admin }}</h2>
+                </v-col>
+                <v-col cols="12" md="6" class="text-right mt-7">
+                    <v-btn @click="createSurvey(adminID)">Create a Survey</v-btn>
+                </v-col>
+            </v-row>
+        </v-container>
+        <v-container class="mb-10">
+            <v-row align="center" justify="center">
                 <v-col cols="12" sm="8" md="4" lg="4">
                     <v-card
                             class="mx-auto"
                             max-width="500"
-                            style="top:-180px"
                     >
                         <v-card-item class="bg-indigo-lighten-5 justify-center">
                             <v-card-title class="align-center">
-                                    <span class="text-h5 text-center ">
-                                        Your Surveys:
-                                    </span>
+                <span class="text-h5 text-center pt-10 ">
+                  Your Surveys:
+                </span>
                             </v-card-title>
-
-
                         </v-card-item>
                     </v-card>
                     <div v-for="(survey,index) in adminSurveys" :key=index >
-                        <v-btn class="mx-auto mb-10"
+                        <v-btn class="mx-auto mt-7"
                                max-width="500"
-                               style="top:-100px"
                                @click="viewAnswers(survey.questionnaire_id)"
                         >
                             {{ survey.name }}
                         </v-btn>
                     </div>
-
                 </v-col>
-
-                <h1></h1>
             </v-row>
         </v-container>
     </v-app>
@@ -81,8 +75,8 @@ export default {
         //goToSurveyAdmin(survey) {
         //function that shows you the answers of a survey
         // },
-        createSurvey(admin) {
-            this.$router.push({name:'CreateSurvey' });
+        createSurvey(adminID) {
+            this.$router.push({name:'CreateSurvey', params: {adminID: adminID}});
 
         },
         viewAnswers(questionnaireID){
