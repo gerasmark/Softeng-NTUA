d03d30d80c9ec7c8e61e43f8819dd9e819e2d521
KonstantinosKatsigiannis <el19127@mail.ntua.gr>
2023-02-15 17:51:25 +0200

    db dump

--- test/functionalapi.test.js
+++ test/functionalapi.test.js
@@ -101,6 +101,16 @@ describe('Getsessionanswers Endpoint', () => {
 });
 
 describe('Healthcheck Endpoint', () => {
+    // before((done) => {
+    //   // Connect to your database
+    //   mongoose.connect('mongodb://localhost:27017/myapp', { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    //     if (err) {
+    //       console.error(err);
+    //       process.exit(1);
+    //     }
+    //     done();
+    //   });
+    // });
     it("Should return status code 200", (done) => {
         request(app)
         .get('/intelliq_api/admin/healthcheck')
