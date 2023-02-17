2c2bedbb91c6d7f5e2a1b9ea800e37d08e8ea7a8
ntua-el19181 <el19181@mail.ntua.gr>
2023-02-17 16:03:20 +0200

    back.js?? double

--- api-backend/back.js
+++ api-backend/back.js
@@ -33,36 +33,36 @@ router.get('/', surveyController.getSurveys);
 router.post('/', surveyController.createSurvey);
 router.put('/:id', surveyController.updateSurvey);
 
-const router = express.Router();
-
-// define routes
-router.get('/', (req, res) => {
-  // handle GET request
-});
-
-router.post('/', (req, res) => {
-  // handle POST request
-});
-
-router.put('/:id', (req, res) => {
-  // handle PUT request
-});
-
-router.delete('/:id', (req, res) => {
-  // handle DELETE request
-});
 
 
-
-
-
-
-/////////////////////////////////////////////////////////
-
-//Link the controllers
-
-const surveyController = require('./controllers/survey');
-
-router.get('/', surveyController.getSurveys);
-router.post('/', surveyController.createSurvey);
-router.put('/:id', surveyController.updateSurvey);
+// // define routes
+// router.get('/', (req, res) => {
+//   // handle GET request
+// });
+//
+// router.post('/', (req, res) => {
+//   // handle POST request
+// });
+//
+// router.put('/:id', (req, res) => {
+//   // handle PUT request
+// });
+//
+// router.delete('/:id', (req, res) => {
+//   // handle DELETE request
+// });
+//
+//
+//
+//
+//
+//
+// /////////////////////////////////////////////////////////
+//
+// //Link the controllers
+//
+// const surveyController = require('./controllers/survey');
+//
+// router.get('/', surveyController.getSurveys);
+// router.post('/', surveyController.createSurvey);
+// router.put('/:id', surveyController.updateSurvey);
