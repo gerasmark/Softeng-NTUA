415fb5899c4adb8ce7f3ae3cd5fd55c701fbee7f
ntua-el19181 <el19181@mail.ntua.gr>
2023-02-17 15:44:57 +0200

    back.js??

--- api-backend/back.js
+++ api-backend/back.js
@@ -32,3 +32,37 @@ const surveyController = require('./controllers/survey');
 router.get('/', surveyController.getSurveys);
 router.post('/', surveyController.createSurvey);
 router.put('/:id', surveyController.updateSurvey);
+
+const router = express.Router();
+
+// define routes
+router.get('/', (req, res) => {
+  // handle GET request
+});
+
+router.post('/', (req, res) => {
+  // handle POST request
+});
+
+router.put('/:id', (req, res) => {
+  // handle PUT request
+});
+
+router.delete('/:id', (req, res) => {
+  // handle DELETE request
+});
+
+
+
+
+
+
+/////////////////////////////////////////////////////////
+
+//Link the controllers
+
+const surveyController = require('./controllers/survey');
+
+router.get('/', surveyController.getSurveys);
+router.post('/', surveyController.createSurvey);
+router.put('/:id', surveyController.updateSurvey);
